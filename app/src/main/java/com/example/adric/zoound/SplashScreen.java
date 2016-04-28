785003cf3cd62325606cacdd077a36d686e83ff6
AdrienCnt <adrien.contu@gmail.com>
2016-04-28 13:43:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/adric/zoound/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.adric.zoound;
+
+/**
+ * Created by adric on 13/04/2016.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
+
